da42d012a35c8c08c9d6a6d9ca6014a65052bd4b
jeissoni <jeissoni_22@hotmail.com>
2022-04-07 17:15:46 -0500

    add configuration for the use of .env file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,13 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+
+require('dotenv').config()
+
+const projeId=process.env.INFURA_PROJECT_ID
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY
+const etherscanApi = process.env.ETHERSCAN_API_KEY;
+
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -18,4 +24,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+
+  networks:{
+
+    hardhat:{
+      forking:{
+        url: `https://rinkeby.infura.io/v3/${projeId}`
+      }
+    },
+
+    rinkeby:{
+      url:`https://rinkeby.infura.io/v3/${projeId}`,
+      accounts:[
+        privateKey
+      ]
+    }
+  },
+
+  etherscan: {
+    apiKey: etherscanApi
+  }
+
 };
