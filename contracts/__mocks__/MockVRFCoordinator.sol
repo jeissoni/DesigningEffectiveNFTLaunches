626cd49cd56499c1df778330c2ae6c760449f0c4
jeissoni <jeissoni_22@hotmail.com>
2022-04-22 17:54:11 -0500

    add file for local oracle tests

--- contracts/__mocks__/MockVRFCoordinator.sol
+++ contracts/__mocks__/MockVRFCoordinator.sol
@@ -2,18 +2,24 @@
 pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol"; // Chainlink VRF
+import "hardhat/console.sol";
 
 contract MockVRFCoordinator {
-    uint256 internal counter = 0;
 
-    function requestRandomWords(
+    uint256 internal counter = 0;    
+
+    function requestRandomness(
        bytes32 _keyHash, 
        uint256 _fee
-    ) external returns (uint256 requestId) {
+    ) external returns (bytes32 requestId) {
+        console.log("VRF");
         VRFConsumerBase consumer = VRFConsumerBase(msg.sender);
-        uint256[] memory randomWords = new uint256[](1);
-        randomWords[0] = counter;
+        // uint256[] memory randomWords = new uint256[](1);
+        // randomWords[0] = counter;
         consumer.rawFulfillRandomness(_keyHash, counter);
         counter += 1;
+        return keccak256(abi.encodePacked(_keyHash));
     }
-}
+
+   
+  }
